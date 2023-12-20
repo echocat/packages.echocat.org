1e923dd06e0b623e2474d687f68104f66baae74d
Gregor Noczinski <gregor@noczinski.eu>
2023-12-20 14:08:00 +0100

    Reduce the amount we're reporting the caching of entries.

--- src/packages.ts
+++ src/packages.ts
@@ -63,7 +63,7 @@ export class CompoundPackages implements Packages {
             status: 200,
             headers: {
                 'Content-Type': 'text/yaml',
-                'Cache-Control': `public, max-age=${oneHourInSeconds}, immutable`,
+                'Cache-Control': `public, max-age=${oneMinuteInSeconds * 5}, immutable`,
             },
         };
 
