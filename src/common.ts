05314de0652687cb1d44250593d348887e9a78a3
Gregor Noczinski <gregor@noczinski.eu>
2025-12-02 11:54:12 +0100

    Modernize build. (#1126)
    * Replaced prettier+eslint with biome
    * Replaced jest with vitest
    * Removed no longer needed dependencies

--- src/common.ts
+++ src/common.ts
@@ -1,9 +1,9 @@
-import {KVNamespace} from '@cloudflare/workers-types';
+import type { KVNamespace } from '@cloudflare/workers-types';
 
 export interface Environment {
-    GITHUB_ACCESS_USER: string;
-    GITHUB_ACCESS_TOKEN: string;
-    KV: KVNamespace;
+   GITHUB_ACCESS_USER: string;
+   GITHUB_ACCESS_TOKEN: string;
+   KV: KVNamespace;
 }
 
 export type Url = string;
--- src/handler_maven.ts
+++ src/handler_maven.ts
@@ -1,12 +1,22 @@
-import {Request, Response} from '@cloudflare/workers-types';
-import {Environment} from './common';
-import {Organization, packages, Repository, Type} from './packages';
-import {Router} from './router';
+import type { Request, Response } from '@cloudflare/workers-types';
+import type { Environment } from './common';
+import { type Organization, packages, type Repository, type Type } from './packages';
+import type { Router } from './router';
 
 export class MavenHandler {
-    constructor(private router: Router) {}
+   constructor(private router: Router) {}
 
-    async handle(request: Request, env: Environment, type: Type, organization: Organization, repository: Repository, file: string): Promise<Response> {
-        return (await packages.findMavenFile(request, env, type, organization, repository, file)) || (await this.router.onNotFound(request, env));
-    }
+   async handle(
+      request: Request,
+      env: Environment,
+      type: Type,
+      organization: Organization,
+      repository: Repository,
+      file: string,
+   ): Promise<Response> {
+      return (
+         (await packages.findMavenFile(request, env, type, organization, repository, file)) ||
+         (await this.router.onNotFound(request, env))
+      );
+   }
 }
